235605c71d791bb4909560421e4f77ddd2d663a1
gorgyboy <jlrogb@gmail.com>
2020-12-12 22:10:51 -0500

    Refactor code

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,21 @@
 # Install and configure Nginx server
 
 exec { 'Install Nginx':
-  command  => 'sudo apt-get update && sudo apt-get -y install nginx',
-  provider => shell,
+  command => 'sudo apt-get update && sudo apt-get -y install nginx',
+  path    => ['/usr/bin', '/bin'],
 }
 
 exec { 'Config index.html':
-  command  => 'echo "Holberton School for the win!" | sudo tee /var/www/html/index.html',
-  provider => shell,
+  command => 'echo "Holberton School for the win!" | sudo tee /var/www/html/index.html',
+  path    => ['/usr/bin', '/bin'],
 }
 
 exec { 'Config redirection page':
-  command  => 'sudo sed -i "$(grep -n \'default_server\' /etc/nginx/sites-enabled/default | tail -1 | cut -f1 -d\':\') a\\ \\n\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-enabled/default',
-  provider => shell,
+  command => 'sudo sed -i "$(grep -n \'default_server\' /etc/nginx/sites-enabled/default | tail -1 | cut -f1 -d\':\') a\\ \n\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-enabled/default',
+  path    => ['/usr/bin', '/bin'],
 }
 
 exec { 'Restart Nginx service':
-  command  => 'sudo service nginx restart',
-  provider => shell,
+  command => 'sudo service nginx restart',
+  path    => ['/usr/bin', '/bin'],
 }
