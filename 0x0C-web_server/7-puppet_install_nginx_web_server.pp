8353052230277e46914d49267649154f1377654b
gorgyboy <jlrogb@gmail.com>
2020-12-12 22:01:23 -0500

    Refactor code

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,9 +1,21 @@
 # Install and configure Nginx server
 
 exec { 'Install Nginx':
-  command => 'sudo apt-get update';
-  command => 'sudo apt-get -y install nginx';
-  command => 'echo "Holberton School for the win!" | sudo tee /var/www/html/index.html';
-  command => 'sudo sed -i "$(grep -n \'default_server\' /etc/nginx/sites-enabled/default | tail -1 | cut -f1 -d\':\') a\\ \\n\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-enabled/default';
-  command => 'sudo service nginx restart';
+  command  => 'sudo apt-get update && sudo apt-get -y install nginx',
+  provider => shell,
+}
+
+exec { 'Config index.html':
+  command  => 'echo "Holberton School for the win!" | sudo tee /var/www/html/index.html',
+  provider => shell,
+}
+
+exec { 'Config redirection page':
+  command  => 'sudo sed -i "$(grep -n \'default_server\' /etc/nginx/sites-enabled/default | tail -1 | cut -f1 -d\':\') a\\ \\n\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-enabled/default',
+  provider => shell,
+}
+
+exec { 'Restart Nginx service':
+  command  => 'sudo service nginx restart',
+  provider => shell,
 }
