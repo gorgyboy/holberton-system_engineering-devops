d7b291e52dbaf835caa763240994df9a6ec2dce8
gorgyboy <jlrogb@gmail.com>
2020-12-13 04:19:55 -0500

    Refactor code

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,27 @@
 # Install and configure Nginx server
 
-exec { 'Install Nginx':
-  command => 'sudo apt-get update && sudo apt-get -y install nginx',
-  path    => ['/usr/bin', '/bin'],
+package { 'nginx':
+  ensure => present,
+  name   => 'nginx',
 }
 
-exec { 'Config index.html':
-  command => 'sudo echo "Holberton School for the win!" > /var/www/html/index.html',
-  path    => ['/usr/bin', '/bin'],
+service { 'nginx':
+  ensure     => running,
+  enable     => true,
+  hasrestart => true,
+  require    => Package['nginx'],
+  subscribe  => File_line['redirect'],
 }
 
-exec { 'Config redirection page':
-  command => 'sudo sed -i "/# SSL configuration/ i\\ \\trewrite ^/redirect_me https://www.google.com permanent;\n" /etc/nginx/sites-enabled/default',
-  path    => ['/usr/bin', '/bin'],
+file { '/var/www/html/index.html':
+  ensure  =>  present,
+  path    => '/var/www/html/index.html',
+  content => 'Holberton School for the win!',
 }
 
-exec { 'Restart Nginx service':
-  command => 'sudo service nginx restart',
-  path    => ['/usr/bin', '/bin'],
+file_line { 'redirect':
+  ensure  => present,
+  after   => '\tlisten [::]:80 default_server;',
+  path    => '/etc/nginx/sites-enabled/default',
+  line    => '\trewrite ^/redirect_me/ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
