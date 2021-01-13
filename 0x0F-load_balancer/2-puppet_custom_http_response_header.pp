d1e738d540d68249e403d8d99f7a0d92e8ac503a
gorgyboy <jlrogb@gmail.com>
2021-01-12 22:42:42 -0500

    Refactor manifest

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,10 +1,6 @@
 # Installs en configures a nginx server with a custom header
 
 exec {'nginx':
-  command  => 'sudo apt-get update && \
-  sudo apt-get -y install nginx && \
-  echo "Holberton School for the win!" | sudo tee /var/www/html/index.html && \
-  sudo sed -i "/^.*sendfile.*/i add_header X-Served-By $hostname;" /etc/nginx/nginx.conf && \
-  sudo service nginx restart',
+  command  => 'sudo apt-get update && sudo apt-get -y install nginx && echo "Holberton School for the win!" | sudo tee /var/www/html/index.html && sudo sed -i "/^.*sendfile.*/i add_header X-Served-By $hostname;" /etc/nginx/nginx.conf && sudo service nginx restart',
   provider => shell,
 }
