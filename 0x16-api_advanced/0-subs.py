49d3acb215508fa675744e1912f5a88869e2a6b3
gorgyboy <jlrogb@gmail.com>
2021-02-04 23:29:54 -0500

    Add task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''API Adv. module - Task 0'''
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''
+    Queries the Reddit API and returns the number of total subscribers 
+    for a given subreddit. If an invalid subreddit is given, returns 0.
+    '''
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        return 0
+    else:
+        return response.json().get('data').get('subscribers')
