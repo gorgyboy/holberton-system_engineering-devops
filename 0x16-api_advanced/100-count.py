80f8ca0d13e980716dce1eaff33b2790988789a7
gorgyboy <jlrogb@gmail.com>
2021-02-04 23:50:45 -0500

    Add Adv. task 3

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''API Adv. module - Adv. task 3'''
+
+
+def count_words(subreddit, word_list):
+    '''
+    Queries the Reddit API, parses the title of all hot articles, and prints
+    a sorted count of given keywords (case-insensitive, delimited by spaces).
+
+    Example:
+        Javascript should count as javascript, but java should not.
+    '''
+    pass
